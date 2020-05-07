c7153c1af85ab0b33bce00227ab353d2b96aa5df
felipecarolino <42390580+felipecarolino@users.noreply.github.com>
2020-05-07 17:08:10 -0300

    Update responsive

--- frontend/src/pages/Header/index.js
+++ frontend/src/pages/Header/index.js
@@ -7,26 +7,33 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 export default function Header() {
-    
+
     return (
         <div className="header">
             <div className="header-top">
-                <img className="header-logo" src={logo} alt="Logo inpe" />
-                <h1>Cataclysmic Variables Portal</h1>
-                <div className="header-language">
-                    <a href="#"><img src={brazilThumbnail} alt="Brazil flag thumbnail" /></a>
-                    <a href="#"><img src={usaThumbnail} alt="USA flag thumbnail" /></a>
+                <div className="header-logo">
+                    <img className="logo" src={logo} alt="Logo inpe" />
+                </div>
+                <div className="header-title">
+                    <h1>Cataclysmic Variables Portal</h1>
+                </div>
+                <div className="header-flags">
+                    <a href="#pt-br"><img src={brazilThumbnail} alt="Brazil flag thumbnail" className="header-flag-br" /></a>
+                    <a href="#en-us"><img src={usaThumbnail} alt="USA flag thumbnail" className="header-flag-us" /></a>
                 </div>
             </div>
 
             <div className="header-nav">
-                <Navbar className="header-navbar">
-                    <Nav>
-                        <Nav.Link href="#users">Users</Nav.Link>
-                        <Nav.Link href="#cataclysmic-variables">Cataclysmic Variables</Nav.Link>
-                        <Nav.Link href="#submissions">Submissions</Nav.Link>
-                        <Nav.Link href="#documentation">Documentation</Nav.Link>
-                    </Nav>
+                <Navbar collapseOnSelect expand="sm">
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#users">Users</Nav.Link>
+                            <Nav.Link href="#cataclysmic-variables">Cataclysmic Variables</Nav.Link>
+                            <Nav.Link href="#submissions">Submissions</Nav.Link>
+                            <Nav.Link href="#documentation">Documentation</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
             </div>
         </div>
